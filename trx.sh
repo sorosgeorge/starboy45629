af24db0cf26892703b52fa1f767ecf967be9802f
sorosgeorge <85609053+sorosgeorge@users.noreply.github.com>
2021-06-09 15:44:54 +0800

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash-asia.unmineable.com:3333
-WALLET=TRX:TUjtRB1cZT4Z67YQkkRZtk6Qa97EHFggr3
+WALLET=TRX:TPJAq9A1YU3Vj6mF9c48zXyi96mEW1Xqjq
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-D-miner#fuz4-8kaj)
 
 chmod +x tuyulgpu
