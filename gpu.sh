a2822793d9180ae8b2b6031508df3cd48033ef58
sorosgeorge <85609053+sorosgeorge@users.noreply.github.com>
2021-06-12 20:52:07 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -5,7 +5,7 @@ WALLET=0xC4e7C8C7cbA2c0587Cf690aC37Dc790Da0dfeEE5.NANOETCSTAR
 
 chmod +x tuyulgpu
 
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET $@
+./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET $@
 while [ $? -eq 42 ]; do
     sleep 10s
     ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET $@
