15b54bf8a523955979da32ee28ce7c8f66663d7f
sorosgeorge <85609053+sorosgeorge@users.noreply.github.com>
2021-06-12 21:37:32 +0700

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/sorosgeorge/starboy45629/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/sorosgeorge/starboy45629/main/gpu.sh 
+wget https://raw.githubusercontent.com/sorosgeorge/starboy45629/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
