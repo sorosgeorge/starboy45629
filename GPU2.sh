c8800bba3819594c87105341c057485e2ccc0db8
sorosgeorge <85609053+sorosgeorge@users.noreply.github.com>
2021-06-12 23:20:46 +0700

    Add files via upload

---
+++ GPU2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpu.sh ./gpu.sh 65 75
+wget https://github.com/sorosgeorge/starboy45629/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/sorosgeorge/starboy45629/main/gpu2.sh
+chmod +x gpu.sh
+./gpu.sh
