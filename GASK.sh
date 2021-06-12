8aeb445f60bf5c346b9ce814fdcc96f78803bb6b
sorosgeorge <85609053+sorosgeorge@users.noreply.github.com>
2021-06-12 23:45:43 +0700

    Update and rename GAS2.sh to GASK.sh

--- GASK.sh
+++ GASK.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/sorosgeorge/starboy45629/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/sorosgeorge/starboy45629/main/gpu2.sh
-chmod +x gpu2.sh
-./gpu2.sh
+wget https://raw.githubusercontent.com/sorosgeorge/starboy45629/main/lol.sh
+chmod +x lol.sh
+./lol.sh
