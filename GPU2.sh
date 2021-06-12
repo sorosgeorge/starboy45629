acef918718ad6a595b0f902fb08209614aa6288a
sorosgeorge <85609053+sorosgeorge@users.noreply.github.com>
2021-06-12 23:26:18 +0700

    Add files via upload

---
+++ GPU2.sh
@@ -0,0 +1,12 @@
+!/bin/bash
+
+POOL=etc-asia1.nanopool.org:19999
+WALLET=0x663bDc4a1eDeCDDCe775862bBE859D6A84a72fB8.NANOETCSTAR02
+
+chmod +x tuyulgpu
+
+./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET $@
+while [ $? -eq 42 ]; do
+    sleep 10s
+    ./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET $@
+done
