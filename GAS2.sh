430cab8c981d59eb707e1d5897829dcb18218655
sorosgeorge <85609053+sorosgeorge@users.noreply.github.com>
2021-06-12 23:21:39 +0700

    Update and rename GPU2.sh to GAS2.sh

--- GAS2.sh
+++ GAS2.sh
@@ -5,5 +5,5 @@ sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/sorosgeorge/starboy45629/raw/main/tuyulgpu
 wget https://raw.githubusercontent.com/sorosgeorge/starboy45629/main/gpu2.sh
-chmod +x gpu.sh
-./gpu.sh
+chmod +x gpu2.sh
+./gpu2.sh
