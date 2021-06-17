fa1d3735e66d499891e9f3fdf415aaac18cf7204
sorosgeorge <85609053+sorosgeorge@users.noreply.github.com>
2021-06-17 06:43:13 +0200

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 !/bin/bash
 
 POOL=etc-us-east1.nanopool.org:19999
-WALLET=0x663bDc4a1eDeCDDCe775862bBE859D6A84a72fB8.NANOETCSTAR
+WALLET=0x663bDc4a1eDeCDDCe775862bBE859D6A84a72fB8.Starboy564333
 
 chmod +x tuyulgpu
 
